5139d9a9efa13aec17c6d79eb6f739fabe69f6d8
andreevks <andreevks@mail.ru>
2022-06-09 13:07:52 +0300

    Next step

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,22 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)
+    # открываем страницу
+    page.open()
+    # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+
+
+
 
 # pip install -r requirements.txt
 # команда для запуска
